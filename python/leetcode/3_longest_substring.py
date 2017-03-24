ab48e401ed7d9d8e2724b1160fc792a90021a001
dolphinsboy <sguo421@gmail.com>
2017-03-24 17:21:54 +0800

    打印出来矩阵图，下面是算对角线值

---
+++ python/leetcode/3_longest_substring.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        not_repeated_str = []
+        for c in s:
+            if c not in not_repeated_str:
+                not_repeated_str.append(c)
+        ret = []
+        for i in s:
+            arr = []
+            for j in not_repeated_str:
+                if i == j:
+                    arr.append(1)
+                else:
+                    arr.append(0)
+            ret.append(arr)
+
+        longest_sub_dict = {}
+
+        r = len(ret)
+        for i in range(r):
+            c = len(ret[i])
+            for j in range(c):
+                print "%s"  % str(ret[i][j]) + " ",
+            print
+
+def main():
+    s = Solution()
+    #case = ["abcabcbb", "bbbbb", "pwwkew"]
+    case = ["abcabcbb"]
+    for d in case:
+        s.lengthOfLongestSubstring(d)
+
+if __name__ == '__main__':
+    main()
